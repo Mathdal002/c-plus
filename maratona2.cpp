bc2cca6f8dee5a3489bf1645eb881d3932ab074b
Mathdal002 <90364847+Mathdal002@users.noreply.github.com>
2022-09-12 23:43:25 -0300

    Atividades Iniciais da Maratona de programação
    aqui eu estou postando códigos feitos através de atividades e trabalhos de programação.

---
+++ maratona2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define PI 3.14159
+ 
+ using namespace std;
+ 
+int main() {
+ 
+  double raio, area;
+ 
+  cout <<("%lf", &raio);
+ 
+  area = PI * (raio * raio);
+ 
+  cout <<("A=%.4f\n", area);
+  return 0;
+}
+
